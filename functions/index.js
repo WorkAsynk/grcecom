c6d819ad2d1e8b1ea1976a9763ed50a57003fd9a
dvip1 <dvip.d.patel@slrtce.in>
2023-12-06 14:09:13 +0530

    structuring + booking.js

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,17 @@
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const createOrder = require("./routes/create_order.js");
+const Test = require("./routes/test.js");
+const Booking = require("./routes/booking.js");
 
+app.use(express.json());
 app.use(cors({origin: true}));
-const serviceAccount = require("./permission.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fir-api-9a206..firebaseio.com",
-});
-const db = admin.firestore();
 
-app.get("/hello-world", (req, res) => {
-  return res.status(200).send("Go Fuck Yourself");
-});
-app.post("/api/createOrder", (req, res) => {
-  (async () => {
-    try {
-      await db.collection("deliveryOrder").doc("/" + req.body.orderId + "/")
-          .create({
-            orderId: req.body.orderId,
-            location: req.body.location,
-            status: "pending",
-          });
-      return res.status(200).send("Order Created Sucessfully");
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
+const db = require('./controller/db.js');
 
+app.get("/test", Test); //to make sure the server is running
+app.post("/api/createOrder", createOrder);
+app.post("/api/booking", Booking);
 exports.app = functions.https.onRequest(app);
