dffc1102906bf0bfa3fb50999c4d5d76aa848e5a
dvip1 <dvip.d.patel@slrtce.in>
2023-12-10 13:00:06 +0530

    ratecalculator

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const app = express();
 const createOrder = require("./routes/create_order.js");
 const Test = require("./routes/test.js");
 const Booking = require("./routes/booking.js");
-
+const RateCalculator = require("./routes/ratecalculator.js");
 app.use(express.json());
 app.use(cors({origin: true}));
 
@@ -14,4 +14,7 @@ const db = require('./controller/db.js');
 app.get("/test", Test); //to make sure the server is running
 app.post("/api/createOrder", createOrder);
 app.post("/api/booking", Booking);
+app.post("/api/ratecalculator", RateCalculator);
+
 exports.app = functions.https.onRequest(app);
+//firebase serve --only functions
---
+++ functions/routes/ratecalculator.js
@@ -0,0 +1,46 @@
+const NodeGeocoder = require('node-geocoder');
+const geolib = require('geolib');
+
+const baseFreightUnitPrice = 100; // 1kg = 100rs
+const baseFuelUnitPrice = 50; // 1km = 50rs
+const gstRate = 0.18; // GST rate
+
+const options = {
+    provider: 'openstreetmap'
+};
+
+const geocoder = NodeGeocoder(options);
+
+const RateCalculator = async (req, res) => {
+    try {
+        const {
+            pickupPincode,
+            deliveryPincode,
+            weight,
+            height, 
+            width,  
+        } = req.body;
+
+        const pickupLocation = await geocoder.geocode(pickupPincode);
+        const deliveryLocation = await geocoder.geocode(deliveryPincode);
+
+        const dist = geolib.getDistance(
+            { latitude: pickupLocation[0].latitude, longitude: pickupLocation[0].longitude },
+            { latitude: deliveryLocation[0].latitude, longitude: deliveryLocation[0].longitude }
+        ) / 1000; // Convert from meters to kilometers
+
+        const volume = width*height*weight * baseFreightUnitPrice;
+        const fuelCharges = dist * baseFuelUnitPrice;
+        const totalCharges = volume + fuelCharges;
+        const gst = totalCharges * gstRate;
+
+        const totalChargesIncludingGST = totalCharges + gst;
+
+        res.json({ totalCharges: totalChargesIncludingGST });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Failed to calculate rate', message: error.message });
+    }
+};
+
+module.exports = RateCalculator;
