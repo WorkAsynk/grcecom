1fc2f0cc4e3af5821cf36aab343dcabd65a4d6bb
dvip1 <dvipatwork@outlook.com>
2024-02-05 13:13:11 +0530

    refactor(ecom): fix eslint errors

--- functions/Ecom/viewStatus.js
+++ functions/Ecom/viewStatus.js
@@ -1,43 +1,49 @@
-/*  function viewStatus that handles an HTTP request to view the status of an order in an e-commerce system. */
-const {findData} = require('../controller/find_data.js');
+/*  function viewStatus that handles an HTTP request to view
+ the status of an order in an e-commerce system. */
+const { findData } = require("../controller/find_data.js");
 const viewStatus = async (req, res) => {
-    try {
-        const {
-            awbNumber,
-            referenceNumber,
-            forwardingNumber,
-        } = req.body;
+  try {
+    const { awbNumber, referenceNumber, forwardingNumber } = req.body;
 
-        let docData;
+    let docData;
 
-        if (awbNumber) {
-            docData = await findData("ecomOrder", 'awbNumber', awbNumber);
-        } else if (referenceNumber) {
-            docData = await findData("ecomOrder", 'referenceNumber', referenceNumber);
-        } else if (forwardingNumber) {
-            docData = await findData("ecomOrder", 'forwardingNumber', forwardingNumber);
-        }
-
-        if (docData) {
-            res.status(200).json({
-                ordersStatus: docData.ordersStatus,
-                deliveryData: docData.destinationData,
-                shippingData: docData.consigneeData,
-                pickupData : {
-                    fuelCharges: docData.fuelCharges,
-                    freightCharges: docData.freightCharges,
-                    totalPrice: docData.totalPrice,
-                }
-            });
-        } else {
-            console.log("No such document!");
-            res.status(404).json({ error: "No such document!" });
-        }
+    if (awbNumber) {
+      docData = await findData("ecomOrder", "awbNumber", awbNumber);
     }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Failed to retrieve data", message: error.message });
+    else if (referenceNumber) {
+      docData = await findData("ecomOrder", "referenceNumber", referenceNumber);
     }
+    else if (forwardingNumber) {
+      docData = await findData(
+          "ecomOrder",
+          "forwardingNumber",
+          forwardingNumber,
+      );
+    }
+
+    if (docData) {
+      res.status(200).json({
+        ordersStatus: docData.ordersStatus,
+        deliveryData: docData.destinationData,
+        shippingData: docData.consigneeData,
+        pickupData: {
+          fuelCharges: docData.fuelCharges,
+          freightCharges: docData.freightCharges,
+          totalPrice: docData.totalPrice,
+        },
+      });
+    }
+    else {
+      console.log("No such document!");
+      res.status(404).json({ error: "No such document!" });
+    }
+  }
+  catch (error) {
+    console.log(error);
+    res
+        .status(500)
+        .json({ error: "Failed to retrieve data", message: error.message });
+  }
 };
 
 module.exports = viewStatus;
