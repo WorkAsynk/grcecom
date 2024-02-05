cd3b81884ee988b0b672776c350b4f1615f5fed3
dvip1 <dvipatwork@outlook.com>
2024-02-05 13:24:00 +0530

    refactor(payment): fix eslint payment

--- functions/payment/instance.js
+++ functions/payment/instance.js
@@ -1,7 +1,7 @@
-const razorpay = require('razorpay');
-require('dotenv').config();
-var instance = new razorpay({
-    key_id: process.env.KEY_ID,
-    key_secret: process.env.KEY_SECRET,
+const Razorpay = require("razorpay");
+require("dotenv").config();
+const instance = new Razorpay({
+  key_id: process.env.KEY_ID,
+  key_secret: process.env.KEY_SECRET,
 });
 module.exports = instance;
--- functions/payment/paymentRoutes.js
+++ functions/payment/paymentRoutes.js
@@ -1,54 +1,57 @@
-const { createOrder } = require('./order');
-const crypto = require('crypto');
-require('dotenv').config();
-const findData = require('../controller/find_data');
-const {saveUser} = require('../controller/CRUD');
+const { createOrder } = require("./order");
+const crypto = require("crypto");
+require("dotenv").config();
+const findData = require("../controller/find_data");
+const { saveUser } = require("../controller/CRUD");
+// eslint-disable-next-line new-cap
 const Router = require("express").Router();
 
 const createOrderRoute = async (req, res) => {
-    const { amount, currency, receipt } = req.body;
+  const { amount, currency, receipt } = req.body;
 
-    try {
-        const order = await createOrder(amount, currency, receipt);
-        const userUID = req.user.uid;
-        
-        const user = await findData('users', 'userUID', userUID);
-        console.log("Retrieved from database: " + user.walletBalance); // Add this line
+  try {
+    const order = await createOrder(amount, currency, receipt);
+    const userUID = req.user.uid;
 
-        if (!user) {
-            return res.status(404).json({ error: 'User not found' });
-        }
-        console.log('Before: '+ user.walletBalance);
-        user.walletBalance = Number(user.walletBalance);
-        console.log("Before: " + user.walletBalance);
-        console.log("Amount: " + amount);
-        user.walletBalance += Number(amount) / 100;
-        console.log("After: " + user.walletBalance);
-        user.walletBalance = user.walletBalance.toString();
+    const user = await findData("users", "userUID", userUID);
+    console.log("Retrieved from database: " + user.walletBalance);
 
-        await saveUser(user, userUID);
-        res.json(order);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: 'Error creating order' });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
+    console.log("Before: " + user.walletBalance);
+    user.walletBalance = Number(user.walletBalance);
+    console.log("Before: " + user.walletBalance);
+    console.log("Amount: " + amount);
+    user.walletBalance += Number(amount) / 100;
+    console.log("After: " + user.walletBalance);
+    user.walletBalance = user.walletBalance.toString();
+
+    await saveUser(user, userUID);
+    res.json(order);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Error creating order" });
+  }
 };
 const successRoute = (req, res) => {
-    const secret = process.env.KEY_SECRET;
-    const { orderCreationId, razorpayPaymentId, razorpaySignature } = req.body;
-    const generatedSignature = crypto
-        .createHmac('sha256', secret)
-        .update(orderCreationId + '|' + razorpayPaymentId)
-        .digest('hex');
-    if (generatedSignature === razorpaySignature) {
-        res.status(200).json({ status: 'success' });
-    } else {
-        res.status(402).json({ status: 'failure' });
-    }
+  const secret = process.env.KEY_SECRET;
+  const { orderCreationId, razorpayPaymentId, razorpaySignature } = req.body;
+  const generatedSignature = crypto
+      .createHmac("sha256", secret)
+      .update(orderCreationId + "|" + razorpayPaymentId)
+      .digest("hex");
+  if (generatedSignature === razorpaySignature) {
+    res.status(200).json({ status: "success" });
+  }
+  else {
+    res.status(402).json({ status: "failure" });
+  }
 };
 
 const failureRoute = (req, res) => {
-    res.status(400).json({ status: 'failed' });
+  res.status(400).json({ status: "failed" });
 };
 
 Router.post("/create", createOrderRoute);
